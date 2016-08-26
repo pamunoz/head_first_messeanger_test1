ee7304f837693b30f0f5bbace0ab1d2df40fe05e
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-26 17:33:12 -0300

    Add a chooser to the Intent and comments to explain

--- app/src/main/java/com/pfariasmunoz/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/pfariasmunoz/messenger/CreateMessageActivity.java
@@ -16,11 +16,21 @@ public class CreateMessageActivity extends Activity {
 
     // Call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
+        // get the EditText view of the layout with the id: message
         EditText messageView = (EditText) findViewById(R.id.message);
+        // get the text added by the user in the EditText
         String messageText = messageView.getText().toString();
+        // Create a new Intent with an action of SEND
         Intent intent = new Intent(Intent.ACTION_SEND);
+        // set the MIME type for the Intent
         intent.setType("text/plain");
+        // Add to the Intent the text from the EditText view
         intent.putExtra(Intent.EXTRA_TEXT, messageText);
-        startActivity(intent);
+        // get the text form a string resource
+        String chooserTitle = getString(R.string.chooser);
+        // wrap the Intent in a chooser so Android always ask for with app to choose
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        // Start the activity of whatever app is available
+        startActivity(chosenIntent);
     }
 }
